2f3bdf606f23b27914dfabebaf3d6b57c2a77d8e
unknown <deekshabisht16@gmail.com>
2022-11-17 11:49:11 +0530

    fourth commit

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,10 +1,12 @@
-import React from "react";
-import { Spin } from 'antd';
+import React, { useState } from "react";
+import { Modal, Spin } from "antd";
 import { EditOutlined, HeartOutlined, DeleteOutlined } from "@ant-design/icons";
 import { Card } from "antd";
+import Modals from "./Modals";
 const { Meta } = Card;
 export default function Cards(props) {
-  return(
+  const [opens, setOpens] = useState(false);
+  return (
     <div>
       <div className="container">
         {props.data.map((element, id) => {
@@ -22,7 +24,7 @@ export default function Cards(props) {
                 }
                 actions={[
                   <HeartOutlined key="like" />,
-                  <EditOutlined key="edit" />,
+                  <EditOutlined key="edit" onClick={() => setOpens(true)} />,
                   <DeleteOutlined key="delete" />,
                 ]}
               >
